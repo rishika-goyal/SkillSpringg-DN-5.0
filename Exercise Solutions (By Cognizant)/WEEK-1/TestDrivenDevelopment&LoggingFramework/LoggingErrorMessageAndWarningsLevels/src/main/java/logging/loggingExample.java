58f0e3939d11974f7c19f17e5d0e1c9b9cc5c910
Rishika Goyal <grishika612@gmail.com>
2026-07-23 10:02:20 +0530

    Add exercise solution to Week-1

---
+++ Exercise Solutions (By Cognizant)/WEEK-1/TestDrivenDevelopment&LoggingFramework/LoggingErrorMessageAndWarningsLevels/src/main/java/logging/loggingExample.java
@@ -0,0 +1,32 @@
+// package Exercise Solutions (By Cognizant).WEEK-1.TestDrivenDevelopment&LoggingFramework.LoggingErrorMessageAndWarningsLevels.src.main.java.logging;
+
+// public class loggingExample {
+//   private static final Logger logger =
+//             LoggerFactory.getLogger(LoggingExample.class);
+
+//     public static void main(String[] args) {
+
+//         logger.error("This is an error message");
+
+//         logger.warn("This is a warning message");
+//     }
+// }
+
+
+package logging;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoggingExample {
+
+    private static final Logger logger =
+            LoggerFactory.getLogger(LoggingExample.class);
+
+    public static void main(String[] args) {
+
+        logger.error("This is an error message");
+
+        logger.warn("This is a warning message");
+    }
+}
